6f96d6c37dcbacea5a068a02e2bc75014cf6d600
Josu DelValleVargas <josudelvallevargas@gmail.com>
2023-05-19 02:41:49 +0000

    primera parte del proyecto

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [inputValue, setInputValue] = useState("");
+  const [todos, setTodos] = useState("");
+
+  //   function entradaDatos(event) {
+  //     setInputValue(event.target.value);
+  //   }
+
+  // añadir en un array -> concat
+  // borrar en un array -> filter
+  // actualizar -> map
+
+  return (
+    <div className="container ">
+      <h1 className="text-center">Todos {inputValue}</h1>
+      <div className="shadow">
+        <ul className="list-unstyled ">
+          <li className="border-bottom border-grey p-3">
+            <input
+              className=" w-100 border-0  "
+              type="text"
+              onChange={(e) => setInputValue(e.target.value)}
+              value={inputValue}
+              onKeyDown={(e) =>
+                e.key === "Enter" ? setTodos(todos.concat([inputValue])) : null
+              }
+            />
+          </li>
+          {/* {todos.map((item, index) => (
+            <li className="d-flex justify-content-between p-3 border-bottom border-grey">
+              <p> {item} </p>
+              <button
+                type="button"
+                class="btn-close"
+                disabled
+                aria-label="Close"
+                onClick={() =>
+                  setTodos(
+                    todos.filter((t, currentIndex) => index != currentIndex)
+                  )
+                }
+              ></button>
+            </li>
+          ))} */}
+        </ul>
+        <div className="p-1">{todos.length} item left</div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
