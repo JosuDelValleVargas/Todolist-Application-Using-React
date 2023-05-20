874d58d6c8890e3a756f7c93bd45b079b246d231
JosuDelValleVargas <129107797+JosuDelValleVargas@users.noreply.github.com>
2023-05-20 01:35:47 +0000

    entrega proyecto

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,7 +2,9 @@ import React, { useState } from "react";
 
 const Home = () => {
   const [inputValue, setInputValue] = useState("");
-  const [todos, setTodos] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [hoverIndex, setHoverIndex] = useState(-1);
+
 
   //   function entradaDatos(event) {
   //     setInputValue(event.target.value);
@@ -12,10 +14,12 @@ const Home = () => {
   // borrar en un array -> filter
   // actualizar -> map
 
+
+
   return (
     <div className="container ">
-      <h1 className="text-center">Todos {inputValue}</h1>
-      <div className="shadow">
+      <h1 className="text-center">Todos </h1>
+      <div className="shadow ">
         <ul className="list-unstyled ">
           <li className="border-bottom border-grey p-3">
             <input
@@ -23,31 +27,42 @@ const Home = () => {
               type="text"
               onChange={(e) => setInputValue(e.target.value)}
               value={inputValue}
-              onKeyDown={(e) =>
-                e.key === "Enter" ? setTodos(todos.concat([inputValue])) : null
-              }
+              onKeyDown={(e) => {
+                // e.key === "Enter" ? setTodos(todos.concat(inputValue)) : null
+                if (e.key === "Enter" && inputValue !== "") {
+                  setTodos(todos.concat(inputValue));
+                  setInputValue("");
+                }
+              }}
             />
           </li>
-           {todos.map((item, index) => 
-            <li className="d-flex justify-content-between p-3 border-bottom border-grey">
-              <p> {item} </p>
-              <button
-                type="button"
-                class="btn-close"
-                disabled
-                aria-label="Close"
-                onClick={() =>
-                  setTodos(
-                    todos.filter((t, currentIndex) => index != currentIndex)
-                  )
-                }
-              ></button>
+          {todos.map((item, index) =>
+            <li className="p-3 border-bottom border-grey">
+              <div
+                key={index}
+                onMouseEnter={() => setHoverIndex(index)}
+                onMouseLeave={() => setHoverIndex(-1)}>
+                {item}
+
+                {hoverIndex === index && (
+                  <button
+                    type="button"
+                    class="btn-close btn-outline ps-5 "
+                    aria-label="Close"
+                    onClick={() =>
+                      setTodos(
+                        todos.filter((todos, currentIndex) => index != currentIndex)
+                      )}>
+                  </button>
+                )}
+              </div>
             </li>
-          )} 
+          )}
         </ul>
-        <div className="p-1">{todos.length} item left</div>
+        <div className="ps-1">{todos.length} item left</div>
       </div>
     </div>
+
   );
 };
 
